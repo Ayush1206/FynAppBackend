0d3c09d914cf4f9bad0b1d9fcd52659efa9b0863
AyushM1206 <ayush.mishra@simbiotiktech.com>
2023-07-22 22:02:00 +0530

    initial commit #2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const corsOptions: cors.CorsOptions = {
+    origin: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true, 
+  };
+  app.use(cors(corsOptions));
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
